6fb82188de467beaae30a826a53066b776529887
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-06-11 19:03:31 -0300

    Identity Server

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
@@ -6,15 +7,38 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { };
+            {
+                new ApiScope("api1", "My API")
+            };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new Client[]
+            {
+                new Client {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                },
+                new Client {
+                    ClientId = "web",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"https://localhost:5002/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:5002/signout-callback-oidc"},
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                    },
+                }
+            };
 }
