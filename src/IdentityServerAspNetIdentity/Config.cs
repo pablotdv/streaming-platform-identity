e2e9a52974faa6f85ee56299829b24c912e4e885
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-06-18 13:24:33 -0300

    Revert "Using ASP.NET Core Identity"
    This reverts commit 7248591aeb87f225e1fe8b3a5fcf812d5d15fce6.

--- src/IdentityServerAspNetIdentity/Config.cs
+++ src/IdentityServerAspNetIdentity/Config.cs
@@ -9,45 +9,44 @@ public static class Config
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-            new IdentityResource(){
-                Name = "verification",
-                UserClaims = new List<string> {
-                    JwtClaimTypes.Email,
-                    JwtClaimTypes.EmailVerified
-                }
-            }
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            {
-                new ApiScope("api1", "My API")
-            };
+        {
+            new ApiScope("scope1"),
+            new ApiScope("scope2"),
+        };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
             {
-                new Client {
-                    ClientId = "client",
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = {
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes = { "api1" }
-                },
-                new Client {
-                    ClientId = "web",
-                    ClientSecrets = {new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = {"https://localhost:5002/signin-oidc"},
-                    PostLogoutRedirectUris = {"https://localhost:5002/signout-callback-oidc"},
-                    AllowOfflineAccess = true,
-                    AllowedScopes = {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "verification",
-                        "api1"
-                    },
-                }
-            };
+                ClientId = "m2m.client",
+                ClientName = "Client Credentials Client",
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                AllowedScopes = { "scope1" }
+            },
+
+            // interactive client using code flow + pkce
+            new Client
+            {
+                ClientId = "interactive",
+                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                RedirectUris = { "https://localhost:44300/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope2" }
+            },
+        };
 }
