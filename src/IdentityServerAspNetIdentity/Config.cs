8f86d78c5e52a362cfb6a6a9e47f206fef6ffdde
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-06-18 18:19:00 -0300

    JavaScript applications with a backend

--- src/IdentityServerAspNetIdentity/Config.cs
+++ src/IdentityServerAspNetIdentity/Config.cs
@@ -52,6 +52,26 @@ public static class Config
                         "api1",
                         "color"
                     },
+                },
+                new Client
+                {
+                    ClientId = "bff",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5003/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5003/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
                 }
             };
 }
